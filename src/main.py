afb4d6a7b0c2896bbe59d7c5b334e725c679c778
Alex Hsu <alex9810171@gmail.com>
2023-12-16 03:01:12 +0800

    update

--- src/main.py
+++ src/main.py
@@ -14,6 +14,8 @@ def get_args_parser(add_help=True):
     parser.add_argument('--x_axis', default='', type=str, help='data of x-axis')
     parser.add_argument('--y_axis', default='', type=str, help='data of y-axis')
     parser.add_argument('--x_extend', default=5, type=int, help='draw lower pp curve')
+    parser.add_argument('--x_axis_spacing', default=10, type=int, help='space of x-axis')
+    parser.add_argument('--y_axis_spacing', default=2000, type=int, help='space of y-axis')
     parser.add_argument('--output_dir', default='./result', type=str, help='path to save outputs')
     return parser
     
@@ -21,21 +23,29 @@ def main(args):
     max_x = 1
     max_y = 1
     
+    plt.figure(figsize=(10.8, 5.4))
+    plt.style.use('seaborn-v0_8-whitegrid')
+    
     file_path = os.path.join(args.data_path, "*.csv")
     for file in glob.glob(file_path):
         df = pd.read_csv(file)
         data_x = df[args.x_axis]
         data_y = df[args.y_axis]
+        
         x, y = get_sigmoid_curve(data_x, data_y, args.x_extend)
         #plt.plot(data_x, data_y, 'o', label='data')
         name = os.path.splitext(os.path.basename(file))[0]
         plt.plot(x, y, label=name)
+        
         if max(x) > max_x: max_x = max(x)
         if max(y) > max_y: max_y = max(y)
 
     plt.xlim(0, max_x*1.1)
     plt.ylim(0, max_y*1.1)
+    plt.xticks(np.arange(0, max_x*1.1, args.x_axis_spacing))
+    plt.yticks(np.arange(0, max_y*1.1, args.y_axis_spacing))
     plt.legend(loc='best')
+    
     file_name = 'curve_' + datetime.now().strftime('%Y%m%d_%H%M%S') + '.png'
     path = os.path.join(args.output_dir, file_name)
     os.makedirs(args.output_dir, exist_ok=True)
--- src/plotter.py
+++ src/plotter.py
@@ -4,7 +4,7 @@ from function import sigmoid
 
 def get_sigmoid_curve(x, y, x_extend):
     p0 = [np.median(x), 1, max(y), min(y)]    
-    popt, pcov = curve_fit(sigmoid, x, y, p0)
+    popt, pcov = curve_fit(sigmoid, x, y, p0, maxfev=4000)
     
     x_lower_bound = min(x)-x_extend
     if x_lower_bound < 0: x_lower_bound = 0
