346bee1b2e551300ac631fcee10474da36369aee
Alex Hsu <alex9810171@gmail.com>
2023-12-16 22:19:14 +0800

    update

--- plot.sh
+++ plot.sh
@@ -1,9 +1,9 @@
 python3 src/main.py \
 --data_path data/compare \
 --title R23_Perf/Power_Curve \
---x_axis r23_package_power \
+--x_axis package_power \
 --y_axis r23_point \
---x_lower_bound 9 \
+--x_lower_bound 5 \
 --x_axis_spacing 10 \
 --y_axis_spacing 2000 \
 --output_dir result
